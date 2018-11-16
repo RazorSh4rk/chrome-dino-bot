26dd2eb7a3e6d7852cf4190021f0ebb9780472b4
Razor <RazorSh4rk@users.noreply.github.com>
2018-11-16 17:57:13 +0100

    upload script

---
+++ script.py
@@ -0,0 +1,24 @@
+import pyautogui, time, PIL
+
+jmp_pixel = (119, 359)
+
+def press_spc():
+    print('jump')
+    pyautogui.keyDown('space')
+    pyautogui.keyUp('space')
+
+def count_black():
+    blck = 0 
+    captured = pyautogui.screenshot(region=(245, 300, 30, 90))
+    for i in range(30):
+        for j in range(90):
+            r, g, b = captured.getpixel((i, j))
+            if (r + g + b != (3 * 255)):
+                blck += 1    
+    return blck
+
+while True:
+    bl = count_black()
+    if bl > 60:
+        press_spc()
+    
